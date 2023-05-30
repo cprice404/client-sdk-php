b3d005369c131536ff53958538ee418c0342eb0c
Pete Gautier <pete@momentohq.com>
2023-05-30 12:55:22 -0700

    lambda testing

---
+++ lambdatest/cdk/bin/lambdatest.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { LambdatestStack } from '../lib/lambdatest-stack';
+
+const app = new cdk.App();
+new LambdatestStack(app, 'LambdatestStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  env: { account: '424729227960', region: 'us-west-2' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
---
+++ lambdatest/cdk/lib/lambdatest-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {DockerImageCode} from 'aws-cdk-lib/aws-lambda';
+
+export class LambdatestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'LambdatestQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    new lambda.DockerImageFunction(this, "lambdatest", {
+        code: DockerImageCode.fromImageAsset("../docker"),
+
+    })
+
+  }
+}
